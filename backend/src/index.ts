5d69986c96ae7482284aa5bc393e80c73a4eca50
tiwariParth <243mjparth@gmail.com>
2024-02-24 20:15:45 +0530

    Migrated database added jwt to signup route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,36 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
 
-const app = new Hono();
-
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
 app.get("/api/v1/blog", (c) => {
   return c.text("Hello Hono!");
 });
 
-app.post("/api/v1/signup", (c) => {
-  return c.json({ message: "Signup" });
+app.post("/api/v1/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const body = await c.req.json();
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+    const jwt = sign({ id: user.id }, c.env?.JWT_SECRET);
+
+    return c.text("jwt here");
+  } catch (e) {
+    return c.status(403);
+  }
 });
 
 app.post("/api/v1/signin", (c) => {
