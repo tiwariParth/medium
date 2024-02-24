ff967525746f300adb09a3c46b59383ffb22dbea
tiwariParth <243mjparth@gmail.com>
2024-02-24 19:19:23 +0530

    Created empty routes needing for the project

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,29 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/api/v1/blog", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+app.post("/api/v1/signup", (c) => {
+  return c.json({ message: "Signup" });
+});
+
+app.post("/api/v1/signin", (c) => {
+  return c.json({ message: "Signup" });
+});
+
+app.post("/api/v1/blog", (c) => {
+  return c.json({ message: "Signup" });
+});
+
+app.put("/api/v1/blog", (c) => {
+  return c.json({ message: "Signup" });
+});
+
+app.get("/api/v1/blog/:id", (c) => {
+  return c.text("Hello Hono!");
+});
+
+export default app;
